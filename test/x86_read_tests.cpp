e0828e5caed2b7fb1c49ed80a3ddc4ed12f83218
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2025-07-19 21:49:36 +0100

    Add test cases for adc, adcx and add instructions to x86 read test

--- test/x86_read_tests.cpp
+++ test/x86_read_tests.cpp
@@ -14,6 +14,8 @@
 #include <signal.h>
 #include <sys/user.h>
 
+#include <inttypes.h>
+
 // Here we test the memcorder_enumerate_memory_accesses function by comparing
 // what reads it thinks are going to happen with an expected read that's
 // manually defined for each test, and then we do an additional check to make
@@ -23,19 +25,17 @@
 
 #define BUFFER_SIZE 64
 
-struct ExpectedRead
-{
-	size_t offset;
-	size_t size;
-	uint64_t value;
-};
-
 static unsigned char s_buffer[BUFFER_SIZE];
 
 static MemcorderMemoryAccess s_accesses[MEMCORDER_MAX_MEMORY_ACCESSES_PER_INSTRUCTION];
 static size_t s_access_count = 0;
 
-static ExpectedRead s_expected_read;
+static uint32_t s_expected_offset;
+static uint32_t s_expected_size;
+static uint64_t s_expected_value;
+static uint8_t s_expected_value_8;
+static uint16_t s_expected_value_16;
+static uint32_t s_expected_value_32;
 static bool s_found_expected_read = false;
 
 static uint64_t s_input;
@@ -84,20 +84,32 @@ static void handle_sigtrap(
 	{
 		const MemcorderMemoryAccess* access = &s_accesses[i];
 		long long offset = static_cast<unsigned char*>(access->address) - s_buffer;
-		
-		fprintf(stderr, "\tread %p %d\t\t\t\t0x%llx\n",
+
+#ifdef VERBOSE_X86_READ_TESTS
+		fprintf(stderr, "\tread %p %d\t\t\t\t0x%llx",
 			access->address,
 			access->size,
 			offset);
+#endif
 		
 		if (access->type == MEMCORDER_MEMORY_ACCESS_TYPE_READ
-			&& access->address == s_buffer + s_expected_read.offset
-			&& access->size == s_expected_read.size
-			&& memcmp(access->address, s_buffer + s_expected_read.offset, s_expected_read.size) == 0)
+			&& access->address == s_buffer + s_expected_offset
+			&& access->size == s_expected_size
+			&& memcmp(access->address, &s_expected_value, s_expected_size) == 0)
 		{
 			s_found_expected_read = true;
+			
+#ifdef VERBOSE_X86_READ_TESTS
+			fprintf(stderr, " <---\n");
+			continue;
+#else
 			break;
+#endif
 		}
+		
+#ifdef VERBOSE_X86_READ_TESTS
+		fprintf(stderr, "\n");
+#endif
 	}
 }
 
@@ -133,7 +145,7 @@ static void run_test(
 	// Fill the buffer with a pattern such that reading from two different
 	// addresses in it will yield different results.
 	for (unsigned char i = 0; i < BUFFER_SIZE; i++)
-		s_buffer[i] = i;
+		s_buffer[i] = i + 1;
 	
 	// Setup a signal handler to run after each instruction is executed, so that
 	// we can detect reads from the buffer.
@@ -152,6 +164,12 @@ static void run_test(
 	
 	s_found_expected_read = false;
 	
+	// Copy the expected value into some smaller variables, useful for declaring
+	// the input operands for inline assembly blocks.
+	s_expected_value_8 = static_cast<uint8_t>(s_expected_value);
+	s_expected_value_16 = static_cast<uint16_t>(s_expected_value);
+	s_expected_value_32 = static_cast<uint32_t>(s_expected_value);
+	
 	// Enable trapping after each instruction. Our signal handler will start
 	// getting called after this.
 	set_eflags_trap_bit(true);
@@ -163,6 +181,17 @@ static void run_test(
 	// getting called after this.
 	set_eflags_trap_bit(false);
 	
+	if (run_result_reader)
+		run_result_reader();
+	uint64_t test_output = s_output;
+	switch (s_expected_size)
+	{
+		case 1: test_output &= 0xff; break;
+		case 2: test_output &= 0xffff; break;
+		case 4: test_output &= 0xffffffff; break;
+	}
+	fprintf(stderr, "Test output: 0x%" PRIx64 "\n", test_output);
+	
 	// Make sure that we successfully predicted that the test instructions was
 	// going to perform the expected read.
 	ASSERT_TRUE(s_found_expected_read);
@@ -170,32 +199,33 @@ static void run_test(
 	if (!run_verifier)
 		return;
 	
-	// Make sure the result of said instruction is the same as the result of
-	// running the same instruction with the expected value as input (to make
-	// sure it actually read from the memory location we thought it did).
-	run_result_reader();
-	uint64_t test_output = s_output;
+	// Run the same instruction again, but this time pass the expected input via
+	// registers so that we can compare the output below.
 	memset(&s_input, 0, sizeof(s_input));
-	memcpy(&s_input, &s_expected_read.value, s_expected_read.size);
+	memcpy(&s_input, &s_expected_value, s_expected_size);
 	s_output = s_input;
 	run_verifier();
 	uint64_t verifier_output = s_output;
-	switch (s_expected_read.size)
+	switch (s_expected_size)
 	{
-		case 8: test_output &= 0xff; verifier_output &= 0xff; break;
-		case 16: test_output &= 0xffff; verifier_output &= 0xffff; break;
-		case 32: test_output &= 0xffffffff; verifier_output &= 0xffffffff; break;
+		case 1: verifier_output &= 0xff; break;
+		case 2: verifier_output &= 0xffff; break;
+		case 4: verifier_output &= 0xffffffff; break;
 	}
+	fprintf(stderr, "Expected output: 0x%" PRIx64 "\n", verifier_output);
+	
+	// Make sure the result of said instruction is the same as the result of
+	// running the same instruction with the expected value as input (to make
+	// sure it actually read from the memory location we thought it did).
 	ASSERT_EQ(test_output, verifier_output);
 }
 
-#define X86_READ_TEST(name, expected, test_body, result_reader, verifier) \
+#define X86_READ_TEST(name, offset, size, value, run_test_body, run_result_reader, run_verifier) \
 	TEST(X86Read, name) \
 	{ \
-		s_expected_read = ExpectedRead expected; \
-		void (*run_test_body)() = test_body; \
-		void (*run_result_reader)() = result_reader; \
-		void (*run_verifier)() = verifier; \
+		s_expected_offset = offset; \
+		s_expected_size = size; \
+		s_expected_value = value; \
 		run_test(run_test_body, run_result_reader, run_verifier); \
 	}
 
@@ -203,11 +233,139 @@ static void run_test(
 
 // *****************************************************************************
 // Instructions (A-L)
-// ***************************************************************************** }
+// *****************************************************************************
 
-X86_READ_TEST(ADC_Imm8ToMem8, ({0, 1, 0x00}),
-	([]() { ASM("clc\n adcb $123, %0" : "+m" (s_buffer)); }),
-	([]() { s_output = s_buffer[0]; }),
-	([]() { ASM("clc\n adcb $123, %%al" : "+a" (s_output) :: "memory");  }));
+X86_READ_TEST(ADC_Imm8ToMem8, 0, 1, 0x01,
+	([]() { ASM("clc\n adcb $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint8_t*) s_buffer; }),
+	([]() { ASM("movb $0x01, %%al\n clc\n adcb $0x12, %%al" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm16ToMem16, 0, 2, 0x0201,
+	([]() { ASM("clc\n adcw $0x1234, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x0201, %%ax\n clc\n adcw $0x1234, %%ax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm32ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("clc\n adcl $0x12345678, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x04030201, %%eax\n clc\n adcl $0x12345678, %%eax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm32ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("clc\n adcq $0x12345678, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x0807060504030201, %%rax\n clc\n adcq $0x12345678, %%rax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm8ToMem16, 0, 2, 0x0201,
+	([]() { ASM("clc\n adcw $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x0201, %%ax\n clc\n adcw $0x12, %%ax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm8ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("clc\n adcl $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x04030201, %%eax\n clc\n adcl $0x12, %%eax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Imm8ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("clc\n adcq $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x0807060504030201, %%rax\n clc\n adcq $0x12, %%rax" : "+a" (s_output)); }));
+X86_READ_TEST(ADC_Reg8ToMem8, 0, 1, 0x01,
+	([]() { ASM("movb $0x12, %%bl\n clc\n adcb %%bl, %0" : "+m" (s_buffer) :: "bl"); }),
+	([]() { s_output = *(uint8_t*) s_buffer; }),
+	([]() { ASM("movb $0x12, %%bl\n movb $0x01, %%al\n clc\n adcb %%bl, %%al" : "+a" (s_output) :: "bl"); }));
+X86_READ_TEST(ADC_Reg16ToMem16, 0, 2, 0x0201,
+	([]() { ASM("movw $0x1234, %%bx\n clc\n adcw %%bx, %0" : "+m" (s_buffer) :: "bx"); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x1234, %%bx\n movw $0x0201, %%ax\n clc\n adcw %%bx, %%ax" : "+a" (s_output) :: "bx"); }));
+X86_READ_TEST(ADC_Reg32ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("movl $0x12345678, %%ebx\n clc\n adcl %%ebx, %0" : "+m" (s_buffer) :: "ebx"); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x12345678, %%ebx\n movl $0x04030201, %%eax\n clc\n adcl %%ebx, %%eax" : "+a" (s_output) :: "ebx"); }));
+X86_READ_TEST(ADC_Reg64ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("movq $0x1234567890123456, %%rbx\n clc\n adcq %%rbx, %0" : "+m" (s_buffer) :: "rbx"); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x1234567890123456, %%rbx\n movq $0x0807060504030201, %%rax\n clc\n adcq %%rbx, %%rax" : "+a" (s_output) :: "rbx"); }));
+X86_READ_TEST(ADC_Mem8ToReg8, 0, 1, 0x01,
+	([]() { ASM("movb $0x12, %%al\n clc\n adcb %1, %%al" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movb $0x12, %%al\n clc\n adcb %1, %%al" : "+a" (s_output) : "r" (s_expected_value_8)); }));
+X86_READ_TEST(ADC_Mem16ToReg16, 0, 2, 0x0201,
+	([]() { ASM("movw $0x1234, %%ax\n clc\n adcw %1, %%ax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movw $0x1234, %%ax\n clc\n adcw %1, %%ax" : "+a" (s_output) : "r" (s_expected_value_16)); }));
+X86_READ_TEST(ADC_Mem32ToReg32, 0, 4, 0x04030201,
+	([]() { ASM("movl $0x12345678, %%eax\n clc\n adcl %1, %%eax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movl $0x12345678, %%eax\n clc\n adcl %1, %%eax" : "+a" (s_output) : "r" (s_expected_value_32)); }));
+X86_READ_TEST(ADC_Mem64ToReg64, 0, 8, 0x0807060504030201,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n clc\n adcq %1, %%rax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n clc\n adcq %1, %%rax" : "+a" (s_output) : "r" (s_expected_value)); }));
+
+#ifdef __broadwell__
+X86_READ_TEST(ADCX_Mem32ToReg32, 0, 4, 0x04030201,
+	([]() { ASM("movl $0x12345678, %%eax\n clc\n adcxl %1, %%eax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movl $0x12345678, %%eax\n clc\n adcxl %1, %%eax" : "+a" (s_output) : "r" (s_expected_value_32)); }));
+X86_READ_TEST(ADCX_Mem64ToReg64, 0, 8, 0x0807060504030201,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n clc\n adcxq %1, %%rax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n clc\n adcxq %1, %%rax" : "+a" (s_output) : "r" (s_expected_value)); }));
+#endif
+
+X86_READ_TEST(ADD_Imm8ToMem8, 0, 1, 0x01,
+	([]() { ASM("addb $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint8_t*) s_buffer; }),
+	([]() { ASM("movb $0x01, %%al\n addb $0x12, %%al" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm16ToMem16, 0, 2, 0x0201,
+	([]() { ASM("addw $0x1234, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x0201, %%ax\n addw $0x1234, %%ax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm32ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("addl $0x12345678, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x04030201, %%eax\n addl $0x12345678, %%eax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm32ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("addq $0x12345678, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x0807060504030201, %%rax\n addq $0x12345678, %%rax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm8ToMem16, 0, 2, 0x0201,
+	([]() { ASM("addw $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x0201, %%ax\n addw $0x12, %%ax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm8ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("addl $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x04030201, %%eax\n addl $0x12, %%eax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Imm8ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("addq $0x12, %0" : "+m" (s_buffer)); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x0807060504030201, %%rax\n addq $0x12, %%rax" : "+a" (s_output)); }));
+X86_READ_TEST(ADD_Reg8ToMem8, 0, 1, 0x01,
+	([]() { ASM("movb $0x12, %%bl\n addb %%bl, %0" : "+m" (s_buffer) :: "bl"); }),
+	([]() { s_output = *(uint8_t*) s_buffer; }),
+	([]() { ASM("movb $0x12, %%bl\n movb $0x01, %%al\n addb %%bl, %%al" : "+a" (s_output) :: "bl"); }));
+X86_READ_TEST(ADD_Reg16ToMem16, 0, 2, 0x0201,
+	([]() { ASM("movw $0x1234, %%bx\n addw %%bx, %0" : "+m" (s_buffer) :: "bx"); }),
+	([]() { s_output = *(uint16_t*) s_buffer; }),
+	([]() { ASM("movw $0x1234, %%bx\n movw $0x0201, %%ax\n addw %%bx, %%ax" : "+a" (s_output) :: "bx"); }));
+X86_READ_TEST(ADD_Reg32ToMem32, 0, 4, 0x04030201,
+	([]() { ASM("movl $0x12345678, %%ebx\n addl %%ebx, %0" : "+m" (s_buffer) :: "ebx"); }),
+	([]() { s_output = *(uint32_t*) s_buffer; }),
+	([]() { ASM("movl $0x12345678, %%ebx\n movl $0x04030201, %%eax\n addl %%ebx, %%eax" : "+a" (s_output) :: "ebx"); }));
+X86_READ_TEST(ADD_Reg64ToMem64, 0, 8, 0x0807060504030201,
+	([]() { ASM("movq $0x1234567890123456, %%rbx\n addq %%rbx, %0" : "+m" (s_buffer) :: "rbx"); }),
+	([]() { s_output = *(uint64_t*) s_buffer; }),
+	([]() { ASM("movq $0x1234567890123456, %%rbx\n movq $0x0807060504030201, %%rax\n addq %%rbx, %%rax" : "+a" (s_output) :: "rbx"); }));
+X86_READ_TEST(ADD_Mem8ToReg8, 0, 1, 0x01,
+	([]() { ASM("movb $0x12, %%al\n addb %1, %%al" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movb $0x12, %%al\n addb %1, %%al" : "+a" (s_output) : "r" (s_expected_value_8)); }));
+X86_READ_TEST(ADD_Mem16ToReg16, 0, 2, 0x0201,
+	([]() { ASM("movw $0x1234, %%ax\n addw %1, %%ax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movw $0x1234, %%ax\n addw %1, %%ax" : "+a" (s_output) : "r" (s_expected_value_16)); }));
+X86_READ_TEST(ADD_Mem32ToReg32, 0, 4, 0x04030201,
+	([]() { ASM("movl $0x12345678, %%eax\n addl %1, %%eax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movl $0x12345678, %%eax\n addl %1, %%eax" : "+a" (s_output) : "r" (s_expected_value_32)); }));
+X86_READ_TEST(ADD_Mem64ToReg64, 0, 8, 0x0807060504030201,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n addq %1, %%rax" : "+a" (s_output) : "m" (s_buffer)); }),
+	nullptr,
+	([]() { ASM("movq $0x1234567890123456, %%rax\n addq %1, %%rax" : "+a" (s_output) : "r" (s_expected_value)); }));
 
 #endif
